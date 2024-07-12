e5e6f3c1fbbe108c27fe1b8647a116e46708ae7f
Murkrow <marcocoppola02.mc@gmail.com>
2024-07-12 22:12:47 +0200

    bugfix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,9 +11,11 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware([
-        Middleware::trustProxies(['*']),
-    ])
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: [
+            '*',
+        ]);
+    })
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
