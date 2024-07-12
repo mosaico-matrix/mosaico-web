aa62d4401dfe3177cf6a3ac99ad040a7d83068fa
Murkrow <marcocoppola02.mc@gmail.com>
2024-07-12 21:53:04 +0200

    trust proxy for file upload

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->trustProxies(at: [
+            '*',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
