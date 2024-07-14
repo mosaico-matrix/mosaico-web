366bfca18594d84054d2191a45e9235d7536df71
Murkrow <marcocoppola02.mc@gmail.com>
2024-07-14 09:53:57 +0200

    Fixed docs script

--- scripts/build-docs.sh
+++ scripts/build-docs.sh
@@ -1,14 +1,8 @@
 echo "Building docs"
-cd public/mosaico-docs
-git fetch --all
-git remote set-head origin main
-cd ../..
-git submodule update --remote public/mosaico-docs
-if [ $? -ne 0 ]; then
-    echo "Error pulling mosaico-docs"
-    exit 1
-fi
-
+cd public
+rm -rf mosaico-docs
+git clone  --depth 1 https://github.com/mosaico-matrix/mosaico-docs
+cd mosaico-docs
 
 # Create venv and install dependencies
 python3 -m venv venv
