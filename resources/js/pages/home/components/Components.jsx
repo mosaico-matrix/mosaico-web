11f8cecdec8ce3e7cd0695449eec9714ec492414
murkrow <marcocoppola02.mc@gmail.com>
2024-09-02 15:31:32 +0200

    Rpi image

--- resources/js/pages/home/components/Components.jsx
+++ resources/js/pages/home/components/Components.jsx
@@ -1,5 +1,6 @@
 import { LandingProductFeaturesGrid } from '@/components/landing/LandingProductFeaturesGrid';
 import { LandingProductVideoFeature } from '@/components/landing/LandingProductVideoFeature';
+import { LandingProductFeature } from '@/components/landing/LandingProductFeature';
 
 export default function Components()
 {
@@ -8,27 +9,25 @@ export default function Components()
                 <LandingProductFeaturesGrid
                     title="What you'll need:"
                     containerType="wide"
+
                     description=""
                 >
-                    <LandingProductVideoFeature
+                    <LandingProductFeature
                         title="Raspberry Pi"
                         description=""
-                        autoPlay={true}
-                        videoSrc="https://cache.shipixen.com/features/2-generate-content-with-ai.mp4"
+                        imageSrc={'/images/rpi.webp'}
                     />
 
-                    <LandingProductVideoFeature
-                        title="Matrx Bonnet"
-                        description="Choose from more than 30+ themes or create your own. Upload your logo, set the size and we take care of the rest."
-                        autoPlay={true}
-                        videoSrc="https://cache.shipixen.com/features/3-theme-and-logo.mp4"
+                    <LandingProductFeature
+                        title="Raspberry Pi"
+                        description=""
+                        imageSrc={'/images/rpi.webp'}
                     />
 
-                    <LandingProductVideoFeature
-                        title="Led Matrix"
-                        description="Use our pricing page builder to create a beautiful pricing page. Choose from different layouts and monthly/yearly pricing options. It's as easy as it looks."
-                        autoPlay={true}
-                        videoSrc="https://cache.shipixen.com/features/11-pricing-page-builder.mp4"
+                    <LandingProductFeature
+                        title="Raspberry Pi"
+                        description=""
+                        imageSrc={'/images/rpi.webp'}
                     />
                 </LandingProductFeaturesGrid>
             </>
